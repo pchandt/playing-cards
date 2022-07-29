ba590c2d2d2f1b427cd2f7ad691c398c410f5c4f
pchandt <pcthakuri26@gmail.com>
2022-07-29 13:50:52 -0700

    Moved rank-first comparator as anonymous class in main.

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -22,7 +22,9 @@ public class TestMain {
     deck.sort();
     System.out.println(deck);
 
-    class RankFirstComparator implements Comparator<Card> {
+
+
+    deck.sort(new Comparator<Card>() {
 
       @Override
       public int compare(Card card1, Card card2) {
@@ -32,10 +34,7 @@ public class TestMain {
         }
         return comparison;
       }
-    }
-
-
-    deck.sort(new RankFirstComparator());
+    });
     System.out.println(deck);
 
 
