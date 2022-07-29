41d2b2500eefbbe1cd617951a5fe0e83c48ba1a5
pchandt <pcthakuri26@gmail.com>
2022-07-29 14:27:44 -0700

    Extracted comparator as constant.

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -5,6 +5,10 @@ import java.util.Objects;
 
 public final class Card implements Comparable<Card> {
 
+  private static final Comparator<Card> NATURAL_ORDER_COMPARATOR = Comparator
+      .comparing(Card::getSuit)
+      .thenComparing(Card::getRank);
+
   private final Rank rank;
   private final Suit suit;
   private final String representation;
@@ -51,9 +55,7 @@ public final class Card implements Comparable<Card> {
 
   @Override
   public int compareTo(Card other) {
-    return Comparator
-        .comparing(Card::getSuit)
-        .thenComparing(Card::getRank)
+    return NATURAL_ORDER_COMPARATOR
         .compare(this, other);
   }
 }
